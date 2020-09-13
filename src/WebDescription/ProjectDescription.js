7ec251c154d9ff8418643aaa49b851f0847d65df
Ting <mycentry1986@gmail.com>
2020-09-12 23:12:19 -0400

    typo

--- src/WebDescription/ProjectDescription.js
+++ src/WebDescription/ProjectDescription.js
@@ -12,10 +12,10 @@ const ProjectDescription = () => {
             <Card>
               <CardContent>
                 <p>*  This project is an individual react practice project </p>
-                <p>*  It is built for helping nurse manage tests and patients</p>
+                <p>*  It is built for helping nurse to manage tests and patients</p>
                 <p>*  After login the system, nurse can manage tests by nurse or
                 by patients</p>
-                <p>*  Database is H2 database</p>
+                <p>*  Database is h2 database</p>
                 <p>*  Backend is built by spring-boot, and it is deployed on
                 Alibaba cloud</p>
                 <p><Avatar src={icon_notice}/>Currently, Nurse ID is the only way used to login the system.</p>
