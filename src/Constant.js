302b02e47bbaec99eed7751edefeccc2c5f7a88c
Ting <mycentry1986@gmail.com>
2020-09-14 16:19:04 -0400

    describe the https login method

--- src/Constant.js
+++ src/Constant.js
@@ -1,4 +1,5 @@
-export const SERVER_URL="http://localhost:8080/";
+// export const SERVER_URL="https://localhost:8443/";
+export const SERVER_URL="https://47.88.53.81:8443/"
 export const LOGIN_ID_PASSWORD_EMPTY_ERROR="Nurse ID or password cannot be empty.";
 export const LOGIN_MATCH="match";
 // export const LOGIN_ID_PASSWORD_INCORRECT="incorrect ID or password";
--- src/WebDescription/ProjectDescription.js
+++ src/WebDescription/ProjectDescription.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Grid, Card, CardContent,Avatar } from "@material-ui/core";
-import icon_information from '../assets/icon_information.png';
+import { Grid, Card, CardContent,Typography } from "@material-ui/core";
 import icon_notice from '../assets/icon_notice.png';
 
 const ProjectDescription = () => {
@@ -18,18 +17,33 @@ const ProjectDescription = () => {
                 <p>*  Database is h2 database</p>
                 <p>*  Backend is built by spring-boot, and it is deployed on
                 Alibaba cloud</p>
-                <p><Avatar src={icon_notice}/>Currently, Nurse ID is the only way used to login the system.</p>
+                <Typography color="primary"><p>Currently, Nurse ID is the only way used to login the system.</p></Typography>
               </CardContent>
             </Card>
           </Grid>
           <Grid item xs={12} sm={12} md={12} lg={12}>
             <Card>
               <CardContent>
-                *  There are 1 nurse, 2 patients , 3 testItems, and 3 tests
-                in the database by default<br/>
-                *  The ID and password of the nurse are<br/>
-                       ID:1<br/>
-                       Password:123
+              <p>*  There are 1 nurse, 2 patients , 3 testItems, and 3 tests
+                in the database by default</p>
+                <p>*  The ID and password of the nurse are</p>
+                <div style={{paddingLeft:"5%"}}>
+                <p>ID:1</p>
+                 <p>Password:123</p>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Card>
+              <CardContent>
+              <Typography color="secondary">
+                <p>As the server is using a free SSL invalid certificate, you will be not able to use login for the first time.</p>
+                <p>Please start a new page and go to https://47.88.53.81:8443/</p>
+                <p>then for chrome, just simply type thisisunsafe on the page</p>
+                <p>for safari, click visit website in the advance</p>
+                <p>then you will be able to use the functions of this web page.</p>
+                </Typography>
               </CardContent>
             </Card>
           </Grid>
