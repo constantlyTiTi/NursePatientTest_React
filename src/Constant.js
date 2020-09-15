aa7f0902029a94bf7bfd387929841c2728eae359
Ting <mycentry1986@gmail.com>
2020-09-14 21:57:08 -0400

    change heroku

--- src/Constant.js
+++ src/Constant.js
@@ -1,5 +1,5 @@
 // export const SERVER_URL="https://localhost:8443/";
-export const SERVER_URL="https://47.88.53.81:8443/"
+export const SERVER_URL="https://evening-sea-93595.herokuapp.com/"
 export const LOGIN_ID_PASSWORD_EMPTY_ERROR="Nurse ID or password cannot be empty.";
 export const LOGIN_MATCH="match";
 // export const LOGIN_ID_PASSWORD_INCORRECT="incorrect ID or password";
