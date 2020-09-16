baf87f1324320413c0f61e77ee98f520c755912d
Ting <mycentry1986@gmail.com>
2020-09-15 22:10:45 -0400

    grammar fix

--- src/WebDescription/ProjectDescription.js
+++ src/WebDescription/ProjectDescription.js
@@ -17,8 +17,7 @@ const ProjectDescription = () => {
                 <p>*	Design and build patient and test management system for nurse by using ReactJs and spring boot.</p>
                 <p>*	The Frontend main features are login, register, nurse profile, patient list/details/CRUD, and test list/details/CRUD. Use React router and material UI to build pages. Use RESTfull API to get data.</p>
                 <p>*  The Backend is built by spring boot JPA, maven, memory h2 database, and RESTfull controller to expose API.</p>
-                <p>*  Backend is built by spring-boot, and it is deployed on
-                Heroku cloud</p>
+                <p>*  The backend is deployed to Heroku Cloud, and the frontend is deployed to GitHub</p>
                 <Typography color="primary"><p>Currently, Nurse ID is the only way used to login the system.</p></Typography>
               </CardContent>
             </Card>
